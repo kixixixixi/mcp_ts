905faa16c6d7f73b0856dab9cb7a291bb5ed50ce
kixixixixi <kixixixixi@gmail.com>
2025-04-16 20:12:46 +0900

    Change tool for test

--- src/server.ts
+++ src/server.ts
@@ -7,14 +7,11 @@ const server = new FastMCP({
 })
 
 server.addTool({
-  name: "add",
-  description: "Add two numbers",
-  parameters: z.object({
-    a: z.number(),
-    b: z.number(),
-  }),
-  execute: async (args) => {
-    return String(args.a + args.b)
+  name: "ShowReloadInformation",
+  description: "株式会社リロードの情報を取得します",
+  parameters: z.object({}),
+  execute: async () => {
+    return "株式会社リロードはシステムを通じて新しい選択肢と新しい可能性を与える会社です。リロード（reload）とは、「再び積み込む」という意味です。主にWebでは一度読み込んだデータを更新することを指します。 株式会社リロードでは、リロードという言葉のように存在しているしくみ・サービスを再考し、刷新することを提案することで新しい選択肢と可能性を与えていくことを目指しています。。"
   },
 })
 
